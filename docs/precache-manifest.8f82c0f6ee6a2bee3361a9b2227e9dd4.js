6379ea74b9c28071ff1d7aa1e5ee459eba1a0f45
Fernando Roman <ferromrob@gmail.com>
2020-10-21 19:15:56 -0500

    feat: add file docs

---
+++ docs/precache-manifest.8f82c0f6ee6a2bee3361a9b2227e9dd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb382c5bea5e46c3356cd33fc4b3fcf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20436042a04b84aa1e9e",
+    "url": "/static/css/main.840c0971.chunk.css"
+  },
+  {
+    "revision": "101d13be7494591733c9",
+    "url": "/static/js/2.f920cbb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f920cbb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20436042a04b84aa1e9e",
+    "url": "/static/js/main.46373d9b.chunk.js"
+  },
+  {
+    "revision": "d8b0256436f47258e6f1",
+    "url": "/static/js/runtime-main.6686dce3.js"
+  }
+]);
